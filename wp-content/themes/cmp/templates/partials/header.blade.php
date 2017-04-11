45ab73338d63fa324712670df665d9e8202e9479
Kalene Csefalvay <kalenecsefalvay@Developers-MacBook-Pro.local>
2017-04-11 11:54:35 -0400

    added font awesome, working on mobile version of nav bar

--- wp-content/themes/cmp/templates/partials/header.blade.php
+++ wp-content/themes/cmp/templates/partials/header.blade.php
@@ -9,20 +9,14 @@
   <div class="container">
     <img src="<?= App\asset_path('images/logo--cmp.svg') ?>" alt="" class="site-header--logo"/>
     {{-- <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a> --}}
+
+
+
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
 
-      <ul class="mobile--nav">
-        <p>Plan your visit</p>
-        <li><a href="#">Things to do</a></li>
-        <li><a href="#">Plan a visit</a></li>
-        <li><a href="#">Join and support</a></li>
-        <li><a href="#">Great event spaces</a></li>
-        <li><a href="#">Carnegie Magazine</a></li>
-        <li><a href="#">About Us</a></li>
-      </ul>
       <ul class="nav">
         <li><a href="#">Things to do</a></li>
         <li><a href="#">Plan a visit</a></li>
@@ -33,6 +27,32 @@
       </ul>
 
 
+      <ul class="mobile--nav">
+        <ul class="mobile__plan">
+          <li>Plan your visit </li>
+          <div class="mobile__symbols">
+            <li class="mobile__down"> <i class="fa fa-chevron-down" aria-hidden="true"></i> </li>
+            <li class="mobile__search"> <i class="fa fa-search" aria-hidden="true"></i> </li>
+            <li class="mobile__hamburger"> <i class="fa fa-bars" aria-hidden="true"></i> </li>
+          </div>
+
+        </ul>
+
+
+
+
+        <li><a href="#">Things to do</a></li>
+        <li><a href="#">Plan a visit</a></li>
+        <li><a href="#">Join and support</a></li>
+        <li><a href="#">Great event spaces</a></li>
+        <li><a href="#">Carnegie Magazine</a></li>
+        <li><a href="#">About Us</a></li>
+      </ul>
+
+
     </nav>
+
+
+
   </div>
 </header>

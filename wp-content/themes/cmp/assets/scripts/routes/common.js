78a72cc977637c362b07b44b468ca8a5cf853942
Kalene Csefalvay <kalenecsefalvay@Developers-MacBook-Pro.local>
2017-04-13 16:57:27 -0400

    trying ot figure out animation

--- wp-content/themes/cmp/assets/scripts/routes/common.js
+++ wp-content/themes/cmp/assets/scripts/routes/common.js
@@ -33,7 +33,10 @@ export default {
         //clicked.find.mobilearrow.contianer.html(down)
 
 
-      clicked.next().toggleClass('active')
+      clicked.next().slideToggle(function(){
+        console.log($(this));
+        $(this).toggleClass('active');
+      })
 
 
 

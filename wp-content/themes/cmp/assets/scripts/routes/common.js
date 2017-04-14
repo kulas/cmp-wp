bfd4cec77a3df5776b5ed508e012fdf482a096fe
Amanda <amanda.wilson@carney.co>
2017-04-14 15:55:26 -0400

    merging

--- wp-content/themes/cmp/assets/scripts/routes/common.js
+++ wp-content/themes/cmp/assets/scripts/routes/common.js
@@ -1,6 +1,43 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    $('.nav > .menu-item, .nav--mobile > .menu-item').on('click', function(){
+      $('.sub-menu', this).toggleClass('active')
+    });
+
+
+
+    $('.nav-mobile--toggle').on('click', function(){
+      $('ul.nav--mobile').toggleClass('active');
+    });
+
+    $('ul.nav--mobile li.menu-item').on('click', function(){
+      $('ul.nav--mobile li.menu-item ul.sub-menu');
+    })
+    //
+    // $('.mobile--nav_link').click(function() {
+    //   var clicked = $(this);
+    //   var upArrow = '<i class="fa fa-chevron-up" aria-hidden="true">';
+    //   var downArrow = '<i class="fa fa-chevron-down" aria-hidden="true">'
+    //
+    //   if( clicked && clicked.next().hasClass('active') ) {
+    //     clicked.find('.mobile--arrow__container').html(downArrow);
+    //   }
+    //
+    //   else {
+    //     $('.mobile--nav_link').not(clicked).next().removeClass('active');
+    //     clicked.find('.mobile--arrow__container').html(upArrow);
+    //     $('.mobile--nav_link').not(clicked).find('.mobile--arrow__container').html(downArrow);
+    //   }
+    //
+    //
+    //   clicked.next().toggleClass('active')
+
+    // });
+
+
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- wp-content/themes/cmp/templates/partials/header.blade.php
+++ wp-content/themes/cmp/templates/partials/header.blade.php
@@ -9,17 +9,33 @@
   <div class="container">
     <img src="<?= App\asset_path('images/logo--cmp.svg') ?>" alt="" class="site-header--logo"/>
     {{-- <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a> --}}
+
+
+
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
-      <ul>
-        <li><a href="#">Things to do</a></li>
-        <li><a href="#">Plan a visit</a></li>
-        <li><a href="#">Join and support</a></li>
-        <li><a href="#">Great event spaces</a></li>
 
-      </ul>
+
+
+      <ul class="nav-mobile--toggle">
+         <li>Plan your visit </li>
+         <div class="nav-mobile__symbols">
+           <li class="nav-mobile-symbols__down"> <i class="fa fa-chevron-down" aria-hidden="true"></i>  </li>
+           <li class="nav-mobile-symbols__search"> <i class="fa fa-search" aria-hidden="true"></i> </li>
+           <li class="nav-mobile-symbols__hamburger"> <i class="fa fa-bars" aria-hidden="true"></i> </li>
+         </div>
+       </ul>
+
+
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav--mobile']) !!}
+      @endif
     </nav>
+
+
+
+
   </div>
 </header>

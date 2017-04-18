c3ba233044049824d064467e12f3a8b1a516ce6c
Kalene Csefalvay <kalenecsefalvay@Developers-MacBook-Pro.local>
2017-04-18 15:43:26 -0400

    working on nav overlay

--- wp-content/themes/cmp/assets/scripts/routes/common.js
+++ wp-content/themes/cmp/assets/scripts/routes/common.js
@@ -2,31 +2,30 @@ export default {
   init() {
     // JavaScript to be fired on all pages
 
+    //display menu items on desktop and mobile
     $('.nav > .menu-item, .nav--mobile > .menu-item').on('click', function(){
       var clicked = $(this);
       clicked.toggleClass('active')
-
       $('.nav > .menu-item, .nav--mobile > .menu-item').not(clicked).children().removeClass('active');
       $('.nav > .menu-item, .nav--mobile > .menu-item').not(clicked).removeClass('active');
-
-
       clicked.children().toggleClass('active');
     });
 
-
-
+    //display drop down on mobile
     $('.nav-mobile-symbols__hamburger').on('click', function(){
       $('.plan').removeClass('active');
       $('.nav--mobile__search').removeClass('active');
       $('ul.nav--mobile').toggleClass('active');
     });
 
+    //display search on mobile
     $('.nav-mobile-symbols__search').click(function(){
       $('.nav--mobile').removeClass('active');
       $('.plan').removeClass('active');
       $('.nav--mobile__search ').toggleClass('active');
     });
 
+    //display plan your visit on mobile
     $('.nav-mobile__plan, .nav-mobile-symbols__down').click(function(){
       $('.nav--mobile').removeClass('active');
       $('.nav--mobile__search ').removeClass('active');
--- wp-content/themes/cmp/templates/partials/header.blade.php
+++ wp-content/themes/cmp/templates/partials/header.blade.php
@@ -44,11 +44,14 @@
        </div>
 
 
+
+
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav--mobile']) !!}
       @endif
     </nav>
 
+    
 
 
 

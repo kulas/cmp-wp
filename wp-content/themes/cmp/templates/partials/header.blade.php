e1b708ebef37595698079ead40318ec300dee823
Kalene Csefalvay <kalenecsefalvay@Developers-MacBook-Pro.local>
2017-04-18 16:19:12 -0400

    started working on nav overlay

--- wp-content/themes/cmp/templates/partials/header.blade.php
+++ wp-content/themes/cmp/templates/partials/header.blade.php
@@ -49,11 +49,18 @@
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav--mobile']) !!}
       @endif
+
+
+
     </nav>
 
-    
+
 
 
 
   </div>
 </header>
+
+<div class="overlay">
+
+</div>

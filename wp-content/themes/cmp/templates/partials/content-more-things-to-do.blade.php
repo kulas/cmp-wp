5f0c348ef501ab262738bbec681fbd4a861f1690
Amanda <amanda.wilson@carney.co>
2017-04-26 17:10:27 -0400

    finished learn with us page

---
+++ wp-content/themes/cmp/templates/partials/content-more-things-to-do.blade.php
@@ -0,0 +1,85 @@
+{{--
+Template Name: More Things to Do
+--}}
+
+<div id="more-things-to-do">
+
+  <div class="content-container">
+
+  <div class="section-hr">
+
+    <hr>
+    <div class="section-hr__h5">
+      <h5>More Things to Do</h5>
+    </div>
+
+  </div>
+
+  <div class="activity-container">
+
+      <div class="activity">
+
+        <img class="activity__square-image" src="wp-content/themes/cmp/assets/images/afterdark-square.jpg">
+        <img class="activity__main-image" src="wp-content/themes/cmp/assets/images/afterdark.jpg">
+
+        <div class="activity__text-box">
+          <h4><a href="#">Nights at the Museum</a></h4>
+          <p>Sleepovers, adults-only fun, stargazing, and more!</p>
+        </div>
+
+      </div>
+
+      <hr class="activity__hr" />
+
+      <div class="activity">
+
+        <img class="activity__square-image" src="wp-content/themes/cmp/assets/images/submarine-square.jpg">
+        <img class="activity__main-image" src="wp-content/themes/cmp/assets/images/submarine.jpg">
+
+        <div class="activity__text-box">
+          <h4><a href="#">Exhibitions</a></h4>
+          <p>Dinosaurs and the art of Andy Warhol.
+            Toss in a World War II submarine, original Monets
+            and Degas, life-size casts of some of the world’s
+            great architecture, and the largest exhibits dedicated
+            to robots and the science of sports.
+          </p>
+        </div>
+
+      </div>
+
+      <hr class="activity__hr" />
+
+      <div class="activity">
+
+        <img class="activity__square-image" src="wp-content/themes/cmp/assets/images/butterflies-square.png">
+        <img class="activity__main-image" src="wp-content/themes/cmp/assets/images/butterflies.png">
+
+        <div class="activity__text-box">
+          <h4><a href="#">Explore Our Collections</a></h4>
+          <p>
+            Some 22 million scientific specimens and 40,000
+            art objects are the inspiration for our research,
+            exhibits, and educational programs.
+          </p>
+        </div>
+
+      </div>
+
+      <hr class="activity__hr" />
+
+      <div class="activity">
+
+        <img class="activity__square-image" src="wp-content/themes/cmp/assets/images/egypt-square.jpg">
+        <img class="activity__main-image" src="wp-content/themes/cmp/assets/images/egypt.jpg">
+
+        <div class="activity__text-box">
+          <h4><a href="#">Travel With Us</a></h4>
+          <p>Pack your bags and explore the world with Carnegie Museums experts.</p>
+        </div>
+
+      </div>
+  </div>
+</div>
+
+</div>

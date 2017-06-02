48166b6c1d5b65d876d3d85174bc9183afc85bbb
Ricky Normandeau <ricky.normandeau@carney.co>
2017-06-02 00:00:11 -0400

    globalize hero-header

--- wp-content/themes/cmp/templates/partials/content-featured-exhibits.blade.php
+++ wp-content/themes/cmp/templates/partials/content-featured-exhibits.blade.php
@@ -2,13 +2,16 @@
   Template Name: Featured Exhibits
 --}}
 
+
 <div id="featured-exhibits">
 
-<div class='section-hr-mobile'>
-  <hr><div class='section-hr__h5'>
+<div class='section-hr small-only'>
+  <hr/>
+  <div class='section-hr__h5'>
     <h5>Now Showing</h5>
   </div>
 </div>
+
 
 <?php $count = 1; ?>
 
@@ -17,61 +20,86 @@
  	// loop through the rows of data
   while ( have_rows('exhibit') ) : the_row();
 
+  // hero block
   if ($count==1) {
-    echo "<div id='exhibit-header' class='exhibit-header exhibit'>";
+    ?>
+        <div class="hero-header" style="background-image: url({{ the_sub_field('exhibit_image') }})"></div>
+        <div class='exhibit-container exhibit--hero'>
+          <div class="exhibit__preview">
+
+            <div class="exhibit-preview__summary">
+
+              <h2 class="hero-header__words-box button--link">
+                <a href="{{ the_sub_field('link') }}">{{ the_sub_field('title') }}</a>
+
+              </h2>
+
+              <div class="exhibit-preview__dates">
+                  <p class="start-date">{{ the_sub_field('dates') }}</p>
+              </div>
+
+              <div class="exhibit-preview__summary-text">{{ the_sub_field('summary') }}</div>
+
+            </div>
+
+          </div>
+      </div>
+    <?php
   }
 
   if ($count==2) {
-    echo "<div class='content-container'>";
+  ?>
+    <div class='section-hr large-only'><hr><div class='section-hr__h5'><h5>Now Showing</h5></div></div>
+    <div class='exhibit-container'>
+  <?php
   }
 
+  // callout blocks
   if ($count >= 2) {
-    echo "<div class='exhibit'>"; } ?>
+  ?>
+    <div class='exhibit'>
+      <div class="exhibit__image" style="background-image: url({{ the_sub_field('exhibit_image') }})">
+      </div>
 
-    <div class="exhibit__image" style="background-image: url({{ the_sub_field('exhibit_image') }})">
+      <div class="exhibit__preview">
 
-    </div>
+        <div class="exhibit-preview__summary">
 
-    <div class="exhibit__preview">
+          <div class="exhibit__words-box">
 
-      <div class="exhibit-preview__summary">
+            <div class="exhibit-preview__dates">
 
-        <div class="exhibit__words-box">
+                <p class="start-date">{{ the_sub_field('dates') }}</p>
 
-          <div class="exhibit-preview__dates">
+            </div>
 
-              <p class="start-date">{{ the_sub_field('dates') }}</p>
+            <a href="{{ the_sub_field('link') }}">
+              <h1 class="exhibit-preview__title button--link">
+                {{ the_sub_field('title') }}
+              </h1>
+            </a>
+
+            <div class="exhibit-preview__summary-text">{{ the_sub_field('summary') }}</div>
 
           </div>
 
-          <a href="{{ the_sub_field('link') }}">
-            <h1 class="exhibit-preview__title">
-              {{ the_sub_field('title') }}
-              <img class="exhibit__learn-more-arrow" src="/wp-content/themes/cmp/assets/images/learnmore-arrow.svg"></a>
-            </h1>
-          </a>
-
-          <div class="exhibit-preview__summary-text">{{ the_sub_field('summary') }}</div>
-
         </div>
-
-      <?php if ($count==1)
-          echo "<div class='section-hr'><hr><div class='section-hr__h5'><h5>Now Showing</h5></div></div>" ?>
 
       </div>
 
-    </div>
-
   </div>
 
-<?php if ($count < 2)
-  echo "</div>" ?>
+<?php
+}
+?>
 
-<?php $count += 1 ?>
+
 
 <?php if ($count==6)
     echo "</div>" ?>
 
+
+<?php $count += 1 ?>
 <?php endwhile; else : endif; ?>
 
 </div>

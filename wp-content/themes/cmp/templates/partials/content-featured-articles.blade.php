65e2a855a8779966faa1b76339121104338a57d8
Kalene Csefalvay <kalenecsefalvay@Developers-MacBook-Pro.local>
2017-09-05 12:46:23 -0400

    Adds main accessibility changes to main page

--- wp-content/themes/cmp/templates/partials/content-featured-articles.blade.php
+++ wp-content/themes/cmp/templates/partials/content-featured-articles.blade.php
@@ -21,7 +21,7 @@
     $featured_image_credit = get_media_credit_html($featured_image_id); //media credit for image
   @endphp
 
-  <a href="{{ the_permalink() }}">
+  <a href="{{ the_permalink() }}" aria-label="{{ the_title() }}">
     <div class="hero-header" style="background-image: url('{{ $featured_image_url }}')" title="{{ the_title() }}"></div>
   </a>
 

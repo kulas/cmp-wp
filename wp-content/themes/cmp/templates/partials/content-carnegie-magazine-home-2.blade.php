58ade3a9caa1c324543779b1b80b233f8af6c20a
Jesse Hixson <hixsonj@gmail.com>
2017-11-10 18:12:52 -0500

    Fixes more magazine home spacing

--- wp-content/themes/cmp/templates/partials/content-carnegie-magazine-home-2.blade.php
+++ wp-content/themes/cmp/templates/partials/content-carnegie-magazine-home-2.blade.php
@@ -15,12 +15,12 @@
     <div class="carnegie-magazine__big-picture">
       <a href="{{ the_field('big_picture_link') }}" aria-label="{{get_field('big_picture_title')}}">
         <div class="hero-header" role="img" style="background-image:url('{{ $big_picture_image_url }}')">
-          <div class="media-details">
-            <p class="media-details__caption">@php echo $big_picture_image['caption']; @endphp</p>
-            <p class="media-details__credit">@php echo $big_picture_image_credit; @endphp</p>
-          </div>
         </div>
       </a>
+      <div class="media-details">
+        <p class="media-details__caption">{{ $big_picture_image['caption'] }}</p>
+        <p class="media-details__credit">{{ $big_picture_image_credit }}</p>
+      </div>
       <div class="content-container">
         <h1 class="hero-header__words-box robot">{{ the_field('big_picture_title') }}</h1>
         <div class="content-wrapper">
@@ -33,7 +33,7 @@
     </div>
       <div class="carnegie-magazine__facetime">
         <div class="content-container">
-          <h2 class="facetime-title uppercase-robot">Face Time</h2>
+          <h2 class="facetime-title">Face Time</h2>
           <div class="facetime">
 
           @php

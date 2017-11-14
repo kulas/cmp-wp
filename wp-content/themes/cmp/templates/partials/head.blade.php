34364a58bcc6f0efafa554d0a0d001f49b6724c7
Jesse Hixson <hixsonj@gmail.com>
2017-11-14 10:49:38 -0500

    Adds sumome plugin and js code

--- wp-content/themes/cmp/templates/partials/head.blade.php
+++ wp-content/themes/cmp/templates/partials/head.blade.php
@@ -3,5 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script src="//load.sumome.com/" data-sumo-site-id="3f7cca6539663e3ee3d32bc5c1a2fdd813d90690096a185462109b18137b5916" async="async"></script>
   @php(wp_head())
 </head>

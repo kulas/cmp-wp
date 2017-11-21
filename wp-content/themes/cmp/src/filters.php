60b97b3104d3518be143835a2cedf522ebbcbc58
Patrick Burke <pat.burke@carney.co>
2017-11-21 16:48:59 -0500

    Adds default values for og:image meta tag.

--- wp-content/themes/cmp/src/filters.php
+++ wp-content/themes/cmp/src/filters.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use The_SEO_Framework;
 
 /**
  * Add <body> classes
@@ -64,3 +65,29 @@ add_filter('template_include', function ($template) {
  * Tell WordPress how to find the compiled path of comments.blade.php
  */
 add_filter('comments_template', 'App\\template_path');
+
+/**
+ * Customize og:image meta value with The SEO Framework plugin
+ */
+add_filter('the_seo_framework_og_image_args', function($args) {
+    if (is_numeric($args['post_id'])):
+        $id = $args['post_id'];
+
+        // try the SEO Framework image override for this post
+        if (empty($args['image']) && !empty($url = get_post_meta($id, '_social_image_url', true))):
+            $args['image'] = $url;
+        endif;
+
+        // try featured image
+        if (empty($args['image']) && has_post_thumbnail()):
+            $args['image'] = get_the_post_thumbnail_url($id, 'large');
+        endif;
+
+        // try header image
+        if (empty($args['image']) && !empty($image = get_field('header_image', $id))):
+            $args['image'] = $image['sizes']['large'];
+        endif;
+    endif;
+
+    return $args;
+});

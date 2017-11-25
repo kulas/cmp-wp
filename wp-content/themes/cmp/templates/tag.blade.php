10a82e52b5e36a3e379fd55067c782dd3f4b15b6
Jesse Hixson <hixsonj@gmail.com>
2017-11-24 20:54:03 -0500

    Fixes expert display and tag display

--- wp-content/themes/cmp/templates/tag.blade.php
+++ wp-content/themes/cmp/templates/tag.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.base')
 
 @section('content')
-  @include('partials.page-header')
+  @include('partials.content-carnegie-magazine-nav')
+  <div class="magazine-sub-page">
+    @include('partials.page-header')
 
-  @while(have_posts()) @php(the_post())
-    @include('partials.tag-partial')
-  @endwhile
+    @if(have_posts()):
+      @while(have_posts()) @php(the_post())
+        @include('partials.tag-partial')
+      @endwhile
+    @else
+      <div class="generic-article tag-partial">
+        <article @php(post_class())>
+          <p>No posts yet, check back soon!</p>
+        </article>
+      </div>
+    @endif
+  </div>
 @endsection

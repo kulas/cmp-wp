f0079f6cd0f2a1d9e3d16baa1faab26fb8fed470
Patrick Burke <pat.burke@carney.co>
2017-11-30 11:20:44 -0500

    Redirects page using Current Issue template to latest published Issue post. Removes unneeded Current Issue field group and updates Sidebar field group.

--- wp-content/themes/cmp/templates/template-carnegie-magazine-current.blade.php
+++ wp-content/themes/cmp/templates/template-carnegie-magazine-current.blade.php
@@ -1,15 +1,17 @@
 {{--
   Template Name: Carnegie Magazine Current Issue
 --}}
+@php
+  // Get latest issue
+  $issue = get_posts(array(
+    'post_type'      => 'issue', // set the post type to issue
+    'posts_per_page' => 1, // include all issues
+    'no_found_rows'  => true, // no pagination necessary so improve efficiency of loop
+    'orderby'        => 'meta_value',
+    'meta_key'       => 'issue_number'
+  ));
 
-@extends('layouts.base')
-
-@section('content')
-
-  @include('partials.content-carnegie-magazine-nav')
-  @include('partials.content-carnegie-magazine-current-1')
-  @include('partials.content-carnegie-magazine-current-2')
-  @include('partials.content-carnegie-magazine-current-3')
-  @include('partials.content-carnegie-magazine-current-4')
-
-@endsection
+  // 302 Redirect to latest issue (or magazine home if no issues found)
+  $redirect_url = !empty($issue) ? get_permalink($issue[0]) : site_url('/carnegie-magazine/') ;
+  header("Location: $redirect_url", true, 302);
+@endphp

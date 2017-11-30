84f0086f997b262ae9a54231840fca1f6129c025
Patrick Burke <pat.burke@carney.co>
2017-11-30 11:07:38 -0500

    Adds hero header to magazine archive template.

--- wp-content/themes/cmp/templates/partials/content-archive.blade.php
+++ wp-content/themes/cmp/templates/partials/content-archive.blade.php
@@ -2,38 +2,57 @@
   Carnegie Magazine Archives Page
 --}}
 
+@php
+  if (have_posts()) : while (have_posts()) : the_post();
+
+  $image = get_field('header_image');
+  $image_id = $image['id'];
+  $image_url = $image['url'];
+  $image_credit = App\get_media_credit_html($image_id, false);
+
+@endphp
+
+@if ($image)
+  <div class="basic-header">
+    <div class="hero-header" role="img" aria-label="{{ the_title() }}" style="background-image:url('{{ $image_url }}')">
+    </div>
+    <div class="media-details">
+      <p class="media-details__caption">@php echo $image['caption']; @endphp</p>
+      <p class="media-details__credit">@php echo $image_credit; @endphp</p>
+    </div>
+  </div>
+@endif
+
 <div class="content-container">
+  <h1 class="hero-header__words-box">{{ the_title() }}</h1>
+
   <div class="magazine-archive">
     <div class="issue">
       <div class="text">
-        @php
-          if ( have_posts() ) :
-          while ( have_posts() ) :
-          the_post();
-        @endphp
-
         {{ the_content() }}
-
-        @php
-          endwhile; else: endif;
-        @endphp
       </div>
 
       {{-- Obtains list of recently Archived magazine issues, loops through these issues to show cover story + blurb --}}
       @php
         $issues = new WP_Query(array(
           'post_type'      => 'issue', // set the post type to issue
-          'posts_per_page' => 10, // number of posts to show
+          'posts_per_page' => -1, // include all issues
           'no_found_rows'  => true, // no pagination necessary so improve efficiency of loop
           'orderby'        => 'meta_value',
           'meta_key'       => 'issue_number'
         ));
 
-        if ( $issues->have_posts() ) : while ( $issues->have_posts() ) : $issues->the_post();
+        $index = 0;
+        if ( $issues->have_posts() ) : while ( $issues->have_posts() ):
+          if ($index > 1):
+            break;
+          else:
+            $issues->the_post();
+          endif;
       @endphp
 
       <div class="issue__cover">
-        {{ the_post_thumbnail('large') }}
+        <a href="{{ the_permalink() }}">{{ the_post_thumbnail('large') }}</a>
       </div>
 
       <div class="issue__content">
@@ -50,7 +69,7 @@
           @endphp
 
             <a href="{{ the_permalink() }}">
-              <h2 class="black-link robot">{{ the_title() }}</h2>
+              <h3 class="sans-serif">{{ the_title() }}</h2>
             </a>
             <p>{{ the_excerpt() }}</p>
             <p class="author">{{ the_field('author') }}</p>
@@ -61,8 +80,10 @@
       </div>
 
       @php
+        $index++;
         endwhile; endif; //resets postdata for the issue.
         wp_reset_postdata();
+        $issues->rewind_posts();
       @endphp
 
     </div>
@@ -96,3 +117,7 @@
     </div>
   </div>
 </div>
+
+@php
+  endwhile; endif;
+@endphp

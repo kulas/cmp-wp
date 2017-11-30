fb11fdfe86b5d3f9c97ce785934702e61be6a848
Patrick Burke <pat.burke@carney.co>
2017-11-30 16:33:55 -0500

    Adds issue type option and modifies template to display archived issues in an iframe. Adds issue year/season fields and displays issue archive by year.

--- wp-content/themes/cmp/templates/partials/content-archive.blade.php
+++ wp-content/themes/cmp/templates/partials/content-archive.blade.php
@@ -32,23 +32,22 @@
         {{ the_content() }}
       </div>
 
-      {{-- Obtains list of recently Archived magazine issues, loops through these issues to show cover story + blurb --}}
       @php
         $issues = new WP_Query(array(
           'post_type'      => 'issue', // set the post type to issue
           'posts_per_page' => -1, // include all issues
           'no_found_rows'  => true, // no pagination necessary so improve efficiency of loop
           'orderby'        => 'meta_value',
-          'meta_key'       => 'issue_number'
+          'meta_key'       => 'issue_number',
+          'meta_query'     => array(
+            array(
+              'key'   => 'issue_type',
+              'value' => 'full'
+            )
+          )
         ));
 
-        $index = 0;
-        if ( $issues->have_posts() ) : while ( $issues->have_posts() ):
-          if ($index > 9):
-            break;
-          else:
-            $issues->the_post();
-          endif;
+        if ($issues->have_posts()) : while ($issues->have_posts()): $issues->the_post();
       @endphp
 
       <div class="issue__cover">
@@ -80,12 +79,62 @@
       </div>
 
       @php
-        $index++;
         endwhile; endif; //resets postdata for the issue.
         wp_reset_postdata();
-        $issues->rewind_posts();
+
+        $issues = get_posts(array(
+          'post_type'      => 'issue', // set the post type to issue
+          'posts_per_page' => -1, // include all issues
+          'no_found_rows'  => true, // no pagination necessary so improve efficiency of loop
+          'orderby'        => 'meta_value',
+          'meta_key'       => 'issue_number',
+          'meta_query'     => array(
+            array(
+              'key'   => 'issue_type',
+              'value' => 'archived'
+            )
+          )
+        ));
       @endphp
 
+      @if (!empty($issues))
+        @php
+          // sort issues by year
+          $issues_by_year = array();
+          foreach ($issues as &$issue):
+            $issue->issue_year = get_field('issue_year', $issue->ID) ?: 'Unknown';
+            $issue->issue_season = get_field('issue_season', $issue->ID) ?: 'Unknown';
+
+            if (!array_key_exists($issue->issue_year, $issues_by_year)):
+              $issues_by_year[$issue->issue_year] = array();
+            endif;
+
+            // add issue to beginning so they display in chronological order
+            array_unshift($issues_by_year[$issue->issue_year], $issue);
+          endforeach;
+        @endphp
+
+        <h2 class="past-issues-heading">Past Issues</h2>
+
+        <ol class="past-issues-list list--blank">
+          @foreach ($issues_by_year as $year => $posts)
+            <li>
+              <h3 class="post-issues-year">{{ $year }}</h3>
+              <ol class="list--inline">
+                @foreach ($posts as $post)
+                  @php(setup_postdata($post))
+                  <li>
+                    <a href="{{ the_permalink() }}">
+                      {{ $post->issue_season }}
+                    </a>
+                  </li>
+                @endforeach
+              </ol>
+            </li>
+          @endforeach
+          @php(wp_reset_postdata())
+        </ul>
+      @endif
     </div>
   </div>
 </div>
--- wp-content/themes/cmp/templates/partials/content-single-issue.blade.php
+++ wp-content/themes/cmp/templates/partials/content-single-issue.blade.php
@@ -1,181 +1,203 @@
-<div class="carnegie-magazine-current-1">
-  <div class="content-container">
-    <div class="current-issue">
-      <div class="current-issue__left">
-        <?= the_post_thumbnail('full'); ?>
-      </div>
-      <div class="current-issue__right">
+@php
+  $issue_type = get_field('issue_type');
+@endphp
 
-        @php
-          global $post;
-          $issue_title = get_the_title();
-          $post_object = get_field('cover_story');
-          if( $post_object ):
-          $post = $post_object;
-          setup_postdata( $post );
-        @endphp
+@if ('archived' === $issue_type)
+  @php($issue_url = get_field('issue_url'))
 
-        <div class="current-issue__cover-story">
-          <h2 class="issue-title">{{ $issue_title }}</h2>
-          <p class="small-uppercase--bold">Cover Story</p>
-          <h3 class="sans-serif"><a href="{{ the_permalink() }}">{{ the_title() }}</a></h3>
-          <p>{{ the_field('quote') }}</p>
-          <p class="author">{{ the_field('author') }}</p>
+  <div class="carnegie-magazine-archived-issue">
+    <div class="content-container">
+      <h2 class="issue-title">{{ the_title() }}</h2>
+      @if ($issue_url)
+        <div class="iframe-wrap">
+          <iframe class="carnegie-magazine-archived-issue-iframe" src="{{ $issue_url }}" frameborder="0"></iframe>
         </div>
+      @endif
+    </div>
+  </div>
 
-        @php
-          wp_reset_postdata(); endif; // Resets postdata for cover story
-        @endphp
+@else
 
-        <div class="magazine-archives">
-          <p class="uppercase-bold center">Magazine Archives</p>
-            <div class="magazine-archives__container">
-              @php
-                $archives = get_posts(array(
-                  'post_type' => 'issue',
-                  'posts_per_page' => 2,
-                  'post__not_in' => array($post->ID),
-                  'orderby' => 'meta_value_num',
-                  'meta_key' => 'issue_number'
-                ));
-              @endphp
+  <div class="carnegie-magazine-current-1">
+    <div class="content-container">
+      <div class="current-issue">
+        <div class="current-issue__left">
+          <?= the_post_thumbnail('full'); ?>
+        </div>
+        <div class="current-issue__right">
 
-              @foreach($archives as $post)
-                @php(setup_postdata($post))
+          @php
+            global $post;
+            $issue_title = get_the_title();
+            $post_object = get_field('cover_story');
+            if( $post_object ):
+            $post = $post_object;
+            setup_postdata( $post );
+          @endphp
 
-                <div class="archive">
-                  <a href="{{ the_permalink() }}">
-                    {{ the_post_thumbnail('large') }}
-                    <p class="small-uppercase--bold center">{{ the_title() }}</p>
-                  </a>
-                </div>
+          <div class="current-issue__cover-story">
+            <h2 class="issue-title">{{ $issue_title }}</h2>
+            <p class="small-uppercase--bold">Cover Story</p>
+            <h3 class="sans-serif"><a href="{{ the_permalink() }}">{{ the_title() }}</a></h3>
+            <p>{{ the_field('quote') }}</p>
+            <p class="author">{{ the_field('author') }}</p>
+          </div>
 
-              @endforeach
-              @php(wp_reset_postdata())
+          @php
+            wp_reset_postdata(); endif; // Resets postdata for cover story
+          @endphp
+
+          <div class="magazine-archives">
+            <p class="uppercase-bold center">Magazine Archives</p>
+              <div class="magazine-archives__container">
+                @php
+                  $archives = get_posts(array(
+                    'post_type' => 'issue',
+                    'posts_per_page' => 2,
+                    'post__not_in' => array($post->ID),
+                    'orderby' => 'meta_value_num',
+                    'meta_key' => 'issue_number'
+                  ));
+                @endphp
+
+                @foreach($archives as $post)
+                  @php(setup_postdata($post))
+
+                  <div class="archive">
+                    <a href="{{ the_permalink() }}">
+                      {{ the_post_thumbnail('large') }}
+                      <p class="small-uppercase--bold center">{{ the_title() }}</p>
+                    </a>
+                  </div>
+
+                @endforeach
+                @php(wp_reset_postdata())
+              </div>
+
+              <a class="green-button" style="display: block; text-align:center;" href="/carnegie-magazine/carnegie-magazine-archive">More Archives</a>
             </div>
-
-            <a class="green-button" style="display: block; text-align:center;" href="/carnegie-magazine/carnegie-magazine-archive">More Archives</a>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 
-@if (have_rows('featured_articles'))
-  <div class="carnegie-magazine-current-1 carnegie-magazine-current-2">
-    <div class="content-container">
-      <h2 class="robot-uppercase">Features</h2>
-      <hr/>
-      <div class="features">
-
-        @while (have_rows('featured_articles'))
-          @php
-            the_row();
-            $post_object = get_sub_field('article');
-          @endphp
-
-          @if ($post_object)
-            @php
-              $post = $post_object;
-              setup_postdata($post);
-            @endphp
-
-            <div class="article">
-              <div class="categories">@php(the_tags( '', '', '' ))</div>
-              <a href="{{ the_permalink() }}">
-              <div class="article__image-container">
-                <img src="{{ the_field('square_image') }}" alt="{{ the_title() }}">
-              </div>
-                <h3 class="green-robot-link">{{ the_title() }}</h3>
-              </a>
-              <p>{{ the_excerpt() }}</p>
-              <p class="author">{{ the_field('author') }}</p>
-            </div>
-
-            @php(wp_reset_postdata())
-          @endif
-        @endwhile
-
-      </div>
-    </div>
-  </div>
-@endif
-
-@if (have_rows('also_in_issue'))
-  <div class="carnegie-magazine-current-3">
-    <div class="content-container">
-      <div class="also-in-issue">
-        <h2 class="robot-uppercase">Also in this Issue</h2>
+  @if (have_rows('featured_articles'))
+    <div class="carnegie-magazine-current-1 carnegie-magazine-current-2">
+      <div class="content-container">
+        <h2 class="robot-uppercase">Features</h2>
         <hr/>
+        <div class="features">
 
-        @while (have_rows('also_in_issue'))
-          @php
-            the_row();
-            $post_object = get_sub_field('article');
-          @endphp
-
-          @if ($post_object)
+          @while (have_rows('featured_articles'))
             @php
-              $post = $post_object;
-              setup_postdata($post);
+              the_row();
+              $post_object = get_sub_field('article');
             @endphp
 
-            <div class="also-article">
-              <div class="also-article__left">
-                <div class="article__image-container">
-                  <a href="{{ the_permalink() }}">
-                    <img src="{{ the_field('square_image') }}" alt="{{ the_title() }}">
-                  </a>
-                </div>
-                <p class="feature-type">{{ the_field('feature_type') }}</p>
-              </div>
-              <div class="also-article__right">
+            @if ($post_object)
+              @php
+                $post = $post_object;
+                setup_postdata($post);
+              @endphp
+
+              <div class="article">
                 <div class="categories">@php(the_tags( '', '', '' ))</div>
                 <a href="{{ the_permalink() }}">
+                <div class="article__image-container">
+                  <img src="{{ the_field('square_image') }}" alt="{{ the_title() }}">
+                </div>
                   <h3 class="green-robot-link">{{ the_title() }}</h3>
                 </a>
                 <p>{{ the_excerpt() }}</p>
                 <p class="author">{{ the_field('author') }}</p>
               </div>
-            </div>
 
-            @php(wp_reset_postdata())
-          @endif
-        @endwhile
+              @php(wp_reset_postdata())
+            @endif
+          @endwhile
 
+        </div>
       </div>
     </div>
-  </div>
-@endif
+  @endif
 
-@php
-  $bigpicture_image = get_field('big_picture_image'); //gets full image array
-  $bigpicture_id = $bigpicture_image['id']; //id of image
-  $bigpicture_url = $bigpicture_image['url']; //url of image
-  $bigpicture_credit = App\get_media_credit_html($bigpicture_id, false); //media credit for image
-@endphp
+  @if (have_rows('also_in_issue'))
+    <div class="carnegie-magazine-current-3">
+      <div class="content-container">
+        <div class="also-in-issue">
+          <h2 class="robot-uppercase">Also in this Issue</h2>
+          <hr/>
 
-@if ($bigpicture_image)
-  <div class="carnegie-magazine__big-picture">
-    <a href="{{ the_field('big_picture_link') }}" aria-label="{{ the_field('big_picture_title') }}">
-      <div class="hero-header" role="img" style="background-image:url('{{ $bigpicture_url }}')"></div>
-    </a>
-    <div class="media-details">
-      <p class="media-details__caption">@php echo $bigpicture_image['caption']; @endphp</p>
-      <p class="media-details__credit">@php echo $bigpicture_credit; @endphp</p>
-    </div>
-    <div class="content-container">
-      <h1 class="hero-header__words-box green-robot-link">{{ the_field('big_picture_title') }}</h1>
-      <p>{{ the_field('big_picture_text') }}</p>
-      <hr />
-    </div>
+          @while (have_rows('also_in_issue'))
+            @php
+              the_row();
+              $post_object = get_sub_field('article');
+            @endphp
 
-    <article>
-      <div class="magazine-subscribe">
-        <p class="serif label"><strong>Sign up to receive more stories in your email</strong></p>
-        @include('partials/magazine-signup-form')
+            @if ($post_object)
+              @php
+                $post = $post_object;
+                setup_postdata($post);
+              @endphp
+
+              <div class="also-article">
+                <div class="also-article__left">
+                  <div class="article__image-container">
+                    <a href="{{ the_permalink() }}">
+                      <img src="{{ the_field('square_image') }}" alt="{{ the_title() }}">
+                    </a>
+                  </div>
+                  <p class="feature-type">{{ the_field('feature_type') }}</p>
+                </div>
+                <div class="also-article__right">
+                  <div class="categories">@php(the_tags( '', '', '' ))</div>
+                  <a href="{{ the_permalink() }}">
+                    <h3 class="green-robot-link">{{ the_title() }}</h3>
+                  </a>
+                  <p>{{ the_excerpt() }}</p>
+                  <p class="author">{{ the_field('author') }}</p>
+                </div>
+              </div>
+
+              @php(wp_reset_postdata())
+            @endif
+          @endwhile
+
+        </div>
       </div>
-    </article>
-  </div>
+    </div>
+  @endif
+
+  @php
+    $bigpicture_image = get_field('big_picture_image'); //gets full image array
+    $bigpicture_id = $bigpicture_image['id']; //id of image
+    $bigpicture_url = $bigpicture_image['url']; //url of image
+    $bigpicture_credit = App\get_media_credit_html($bigpicture_id, false); //media credit for image
+  @endphp
+
+  @if ($bigpicture_image)
+    <div class="carnegie-magazine__big-picture">
+      <a href="{{ the_field('big_picture_link') }}" aria-label="{{ the_field('big_picture_title') }}">
+        <div class="hero-header" role="img" style="background-image:url('{{ $bigpicture_url }}')"></div>
+      </a>
+      <div class="media-details">
+        <p class="media-details__caption">@php echo $bigpicture_image['caption']; @endphp</p>
+        <p class="media-details__credit">@php echo $bigpicture_credit; @endphp</p>
+      </div>
+      <div class="content-container">
+        <h1 class="hero-header__words-box green-robot-link">{{ the_field('big_picture_title') }}</h1>
+        <p>{{ the_field('big_picture_text') }}</p>
+        <hr />
+      </div>
+
+      <article>
+        <div class="magazine-subscribe">
+          <p class="serif label"><strong>Sign up to receive more stories in your email</strong></p>
+          @include('partials/magazine-signup-form')
+        </div>
+      </article>
+    </div>
+  @endif
+
 @endif

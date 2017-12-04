05fcc2d9dec7b4016d2a721afd61d6ac39b62b9d
Patrick Burke <pat.burke@carney.co>
2017-12-04 14:21:01 -0500

    Fixes indentation on shortcodes.php.

--- wp-content/themes/cmp/src/shortcodes.php
+++ wp-content/themes/cmp/src/shortcodes.php
@@ -3,53 +3,53 @@
 namespace App;
 
 function specialties_list($atts) {
-  $terms = get_terms( array(
-    'post_type' => 'expert',
-    'taxonomy' => 'specialties',
-    'hide_empty' => true,
-    'orderby' => 'name'
-  ));
+    $terms = get_terms( array(
+        'post_type' => 'expert',
+        'taxonomy' => 'specialties',
+        'hide_empty' => true,
+        'orderby' => 'name'
+    ));
 
-  $specialty_links = array_map(function($specialty) {
-    return '<li><a href="'.get_term_link($specialty).'">'.$specialty->name.'</a></li>';
-  }, $terms);
+    $specialty_links = array_map(function($specialty) {
+        return '<li><a href="'.get_term_link($specialty).'">'.$specialty->name.'</a></li>';
+    }, $terms);
 
-  $output = "<ul>";
-  $output .= implode("", $specialty_links);
-  $output .= "</ul>";
-  return $output;
+    $output = "<ul>";
+    $output .= implode("", $specialty_links);
+    $output .= "</ul>";
+    return $output;
 }
 
 function museums_list($atts) {
-  $terms = get_terms( array(
-    'post_type' => 'expert',
-    'taxonomy' => 'museums',
-    'hide_empty' => true,
-    'orderby' => 'name'
-  ));
+    $terms = get_terms( array(
+        'post_type' => 'expert',
+        'taxonomy' => 'museums',
+        'hide_empty' => true,
+        'orderby' => 'name'
+    ));
 
-  $museum_links = array_map(function($museum) {
-    return '<li><a href="'.get_term_link($museum).'">'.$museum->name.'</a></li>';
-  }, $terms);
+    $museum_links = array_map(function($museum) {
+        return '<li><a href="'.get_term_link($museum).'">'.$museum->name.'</a></li>';
+    }, $terms);
 
-  $output = "<ul>";
-  $output .= implode("", $museum_links);
-  $output .= "</ul>";
-  return $output;
+    $output = "<ul>";
+    $output .= implode("", $museum_links);
+    $output .= "</ul>";
+    return $output;
 }
 
 /**
- * Fixes empty <p> and <br> tags showing before and after shortcodes in the
- * output content.
- */
+* Fixes empty <p> and <br> tags showing before and after shortcodes in the
+* output content.
+*/
 function the_content_shortcode_fix($content) {
-  $array = array(
-      '<p>['    => '[',
-      ']</p>'   => ']',
-      ']<br />' => ']',
-      ']<br>'   => ']'
-  );
-  return strtr($content, $array);
+    $array = array(
+        '<p>['    => '[',
+        ']</p>'   => ']',
+        ']<br />' => ']',
+        ']<br>'   => ']'
+    );
+    return strtr($content, $array);
 }
 add_filter('the_content', 'App\\the_content_shortcode_fix');
 
@@ -63,11 +63,11 @@ add_filter('acf/format_value/name=tab_copy', 'App\\acf_content', 10, 3);
 add_filter('acf/format_value/name=trip_copy', 'App\\acf_content', 10, 3);
 
 /**
- * Register shortcodes
- */
+* Register shortcodes
+*/
 function register_shortcodes() {
-  add_shortcode('specialties_list', 'App\\specialties_list');
-  add_shortcode('museums_list', 'App\\museums_list');
+    add_shortcode('specialties_list', 'App\\specialties_list');
+    add_shortcode('museums_list', 'App\\museums_list');
 }
 add_action('init', 'App\\register_shortcodes');
 

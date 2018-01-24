e6e43385e7b7b00e6f551b856c8055bd9e941eb7
Patrick Burke <pat.burke@carney.co>
2018-01-24 10:41:56 -0500

    Adds "minimal template" option to hide header nav, search bar, and footer.

--- wp-content/themes/cmp/templates/single.blade.php
+++ wp-content/themes/cmp/templates/single.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.base')
 
+@php($minimal_template = get_field('minimal_template'))
+
 @section('content')
-  @include('partials.content-carnegie-magazine-nav')
+  @if (empty($minimal_template))
+    @include('partials.content-carnegie-magazine-nav')
+  @endif
   @while(have_posts()) @php(the_post())
     @include('partials/content-single-'.get_post_type())
   @endwhile

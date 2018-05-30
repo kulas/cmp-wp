5439cda9efdddb56db44c74bfab4c7d60745d7db
Jesse Hixson <hixsonj@gmail.com>
2018-05-30 16:48:27 -0400

    Adds omnilert plugin and table styles

--- wp-content/themes/cmp/templates/partials/content-home.blade.php
+++ wp-content/themes/cmp/templates/partials/content-home.blade.php
@@ -1,1 +1,12 @@
 {{-- Doesn't look like anything to me. --}}
+@php
+  if ( have_posts() ) :
+  while ( have_posts() ) :
+  the_post();
+@endphp
+
+{{ the_content() }}
+
+@php
+  endwhile; endif;
+@endphp
